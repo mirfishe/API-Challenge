5d43f15867d246abe822546cf0f5a54678c6109e
Michael Fisher <mirfishe@gmail.com>
2020-08-12 16:33:34 -0400

    backup

--- index.js
+++ index.js
@@ -7,16 +7,18 @@ const habitatURL = "https://pokeapi.co/api/v2/pokemon-habitat/";
 const colorDropDown= document.getElementById("ddColor");
 const habitatDropDown= document.getElementById("ddHabitat");
 
+const btnSearch= document.getElementById("btnSearch");
+btnSearch.style.display = 'none';
+
 const searchForm = document.getElementById("frmSearch");
 
 const errorHeader = document.getElementById("h2Error");
 const resultsHeader = document.getElementById("h2Results");
+const h3SearchedOn = document.getElementById("h3SearchedOn");
 
-const resultList = document.getElementById("resultList");
-
-//colorDropDown.addEventListener('change', getResults); 
-searchForm.addEventListener('submit', getResults); 
-
+colorDropDown.addEventListener('change', getResults); 
+habitatDropDown.addEventListener('change', getResults); 
+//searchForm.addEventListener('submit', getResults); 
 
 const detailsDiv = document.getElementById("pokemonDetails");
 const pokemonName = document.getElementById("pokemonName");
@@ -33,8 +35,19 @@ const back_shiny_female = document.getElementById("back_shiny_female");
 const pokemonHeight = document.getElementById("pokemonHeight");
 const pokemonWeight = document.getElementById("pokemonWeight");
 
+const pokemonTypes = document.getElementById("pokemonTypes");
+
+const pokemonMoves = document.getElementById("pokemonMoves");
+
+const pokemonAbilities = document.getElementById("pokemonAbilities");
+
+const pokemonForms = document.getElementById("pokemonForms");
+
+const pokemonHeldItems = document.getElementById("pokemonHeldItems");
 
 detailsDiv.style.display = 'none';
+
+let firstLoop = 0;
 
 // BEGIN Code For Testing
 //colorTextBox.value = "blue";
@@ -63,6 +76,14 @@ function addColors(data){
 
     // console.log(data.results);
     let colors = data.results;
+
+    colors.sort((a, b) => {
+        if (a.name > b.name) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
 
     colors.forEach(element => {
         // console.log(element);
@@ -96,6 +117,14 @@ function addHabitats(data){
     // console.log(data.results);
     let habitats = data.results;
 
+    habitats.sort((a, b) => {
+        if (a.name > b.name) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
     habitats.forEach(element => {
         // console.log(element);
         let habitatOption = document.createElement("option");
@@ -105,14 +134,15 @@ function addHabitats(data){
     })
 };
 
-let arrColor = [];
-let arrHabitat = [];
-let arrJoinedItems = [];
+
+
 
 
 // Get the results after the search
 function getResults(e){
     e.preventDefault();
+
+    detailsDiv.style.display = 'none';
 
     if (colorDropDown.value !== "" || habitatDropDown.value !== "") {
 
@@ -123,71 +153,33 @@ function getResults(e){
         //let URL = colorURL + colorTextBox.value;
         // Using the drop down
         let URL;
-
         if (colorDropDown.value != "") {
             URL = colorURL + colorDropDown.value;
-
-            fetch(URL)
-            .then(result => {
-                // console.log(result);
-                return result.json();
-            })
-            .then(data => {
-                // console.log(data);
-                //displayResults(data);
-
-                arrColor = data.pokemon_species;
-                console.log(arrColor);
-                compareArrays();
-                displayResults();
-            })
-            .catch(err => {
-                console.log(err)
-                errorHeader.innerText = err;
-                errorHeader.style.display = 'flex';
-            });
-
-        };
-        
-        if (habitatDropDown.value != "") {
+            h3SearchedOn.innerText = "Searched: Color=" + colorDropDown.value;
+            h3SearchedOn.style.display = 'flex';
+        } else if (habitatDropDown.value != "") {
             URL = habitatURL + habitatDropDown.value;
-
-            fetch(URL)
-            .then(result => {
-                // console.log(result);
-                return result.json();
-            })
-            .then(data => {
-                // console.log(data);
-                //displayResults(data);
-
-                arrHabitat = data.pokemon_species;
-                console.log(arrHabitat);
-                compareArrays();
-                displayResults();
-            })
-            .catch(err => {
-                console.log(err)
-                errorHeader.innerText = err;
-                errorHeader.style.display = 'flex';
-            });
-
+            h3SearchedOn.innerText = "Searched: Habitat=" + habitatDropDown.value;
+            h3SearchedOn.style.display = 'flex';
         };
 
-        // fetch(URL)
-        // .then(result => {
-        //     // console.log(result);
-        //     return result.json();
-        // })
-        // .then(data => {
-        //     // console.log(data);
-        //     displayResults(data);
-        // })
-        // .catch(err => {
-        //     console.log(err)
-        //     errorHeader.innerText = err;
-        //     errorHeader.style.display = 'flex';
-        // });
+        colorDropDown.selectedIndex = 0;
+        habitatDropDown.selectedIndex = 0;
+
+        fetch(URL)
+        .then(result => {
+            // console.log(result);
+            return result.json();
+        })
+        .then(data => {
+            // console.log(data);
+            displayResults(data);
+        })
+        .catch(err => {
+            console.log(err)
+            errorHeader.innerText = err;
+            errorHeader.style.display = 'flex';
+        });
 
     } else {
         errorHeader.innerText = "Please Select An Option";
@@ -197,39 +189,32 @@ function getResults(e){
 
 };
 
-function compareArrays() {
 
-    // remove all items in arrJoinedItems
-    arrJoinedItems = [];
-
-    if (arrColor.length > 0 && arrHabitat.length > 0) {
-        for (c = 0; c < arrColor.length; c++){
-            for (h = 0; h < arrHabitat.length; h++){
-                if (arrColor[c].name == arrHabitat[h].name) {
-                    arrJoinedItems.push(arrColor[c].name);
-                };
-            };
-        };
-    };
-
-    console.log(arrJoinedItems);
-    return arrJoinedItems;
-};
-
-
-function displayResults(){
+function displayResults(data){
     // console.log(data);
 
-    // console.log(data.pokemon_species);
-    //let results = data.pokemon_species;
+    while (resultList.firstChild) { // while the value is not null
+        resultList.removeChild(resultList.firstChild);
+    };
 
-    arrJoinedItems.forEach(element => {
+    // console.log(data.pokemon_species);
+    let results = data.pokemon_species;
+
+    results.sort((a, b) => {
+        if (a.name > b.name) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    results.forEach(element => {
         // console.log(element);
 
         resultsHeader.style.display = 'flex';
 
         let pokemonLI = document.createElement("li");
-        pokemonLI.className = "list-group-item list-group-item-light";
+        //pokemonLI.className = "list-group-item list-group-item-light";
 
         let pokemonA = document.createElement("a");
         //pokemonA.href = element.url;
@@ -261,11 +246,23 @@ function getPokemon(event){
     fetch(URL)
     .then(result => {
         // console.log(result);
-        return result.json();
+        // console.log(result.status);
+
+        if (result.status != 404) {
+            return result.json();
+        } else {
+            console.log(result)
+            errorHeader.innerText = "Pokémon not found.";
+            errorHeader.style.display = 'flex';
+            return result.status
+        };
     })
     .then(data => {
-        //console.log(data);
-        displayPokemon(data);
+        // console.log(data);
+
+        if (data != 404) {
+            displayPokemon(data);
+        }
     })
     .catch(err => {
         console.log(err)
@@ -284,7 +281,7 @@ function displayPokemon(data){
     pokemonName.innerText = data.name;
 
     let sprites = data.sprites;
-    console.log(sprites);
+    //console.log(sprites);
 
     pokemonSprites.style.display = 'flex';
 
@@ -346,8 +343,186 @@ function displayPokemon(data){
     // };
 
 
-    pokemonHeight.innerText = "Height: " + data.height;
-    pokemonWeight.innerText = "Weight: " + data.weight;
+    // pokemonHeight.innerText = "Height: " + data.height;
+    // pokemonWeight.innerText = "Weight: " + data.weight;
+    pokemonHeight.innerText = data.height;
+    pokemonWeight.innerText = data.weight;
+
+
+    let types = data.types;
+    //console.log(types);
+
+    types.sort((a, b) => {
+        if (a.type.name > b.type.name) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    firstLoop = 0;
+    types.forEach(element => {
+        //console.log(element);
+
+        // if (firstLoop === 0) {
+        //     pokemonTypes.innerText += element.type.name;
+        //     firstLoop = 1;
+        // } else {
+        //     pokemonTypes.innerText += ", " + element.type.name;
+        // };
+
+        if (firstLoop !== 0) {
+            pokemonTypes.innerText += ", "
+        } else {
+            pokemonTypes.innerText = "Types: "
+            firstLoop = 1;
+        };
+
+        pokemonTypes.innerText += element.type.name;
+
+    })
+
+
+    let moves = data.moves;
+    //console.log(moves);
+
+    moves.sort((a, b) => {
+        if (a.move.name > b.move.name) {
+        // console.log("A: ", a.move.name);
+        // console.log("B: ", b.move.name);
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    firstLoop = 0;
+    moves.forEach(element => {
+        //console.log(element);
+
+        // if (firstLoop === 0) {
+        //     pokemonMoves.innerText += element.move.name;
+        //     firstLoop = 1;
+        // } else {
+        //     pokemonMoves.innerText += ", " + element.move.name;
+        // };
+
+        if (firstLoop !== 0) {
+            pokemonMoves.innerText += ", "
+        } else {
+            pokemonMoves.innerText = "Moves: "
+            firstLoop = 1;
+        };
+
+        pokemonMoves.innerText += element.move.name;
+
+    })
+
+
+    let abilities = data.abilities;
+    //console.log(abilities);
+
+    abilities.sort((a, b) => {
+        if (a.ability.name > b.ability.name) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    firstLoop = 0;
+    abilities.forEach(element => {
+        //console.log(element);
+
+        // if (firstLoop === 0) {
+        //     pokemonAbilities.innerText += element.ability.name;
+        //     firstLoop = 1;
+        // } else {
+        //     pokemonAbilities.innerText += ", " + element.ability.name;
+        // };
+
+        if (firstLoop !== 0) {
+            pokemonAbilities.innerText += ", "
+        } else {
+            pokemonAbilities.innerText = "Abilities: "
+            firstLoop = 1;
+        };
+
+        if (element.is_hidden) {
+            pokemonAbilities.innerText += element.ability.name + "(Hidden)";
+        } else {
+            pokemonAbilities.innerText += element.ability.name;
+        };
+
+    })
+    
+
+    let forms = data.forms
+    //console.log(forms);
+
+    forms.sort((a, b) => {
+        if (a.name > b.name) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    firstLoop = 0;
+    forms.forEach(element => {
+        //console.log(element);
+
+        // if (firstLoop === 0) {
+        //     pokemonForms.innerText += element.ability.name;
+        //     firstLoop = 1;
+        // } else {
+        //     pokemonForms.innerText += ", " + element.ability.name;
+        // };
+
+        if (firstLoop !== 0) {
+            pokemonForms.innerText += ", "
+        } else {
+            pokemonForms.innerText = "Forms: "
+            firstLoop = 1;
+        };
+
+        pokemonForms.innerText += element.name;
+
+    })
+
+
+    let held_items = data.held_items;
+    //console.log(heldItems);
+
+    held_items.sort((a, b) => {
+        if (a.item.name > b.item.name) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+
+    firstLoop = 0;
+    held_items.forEach(element => {
+        //console.log(element);
+
+        // if (firstLoop === 0) {
+        //     pokemonHeldItems.innerText += element.type.name;
+        //     firstLoop = 1;
+        // } else {
+        //     pokemonHeldItems.innerText += ", " + element.type.name;
+        // };
+
+        if (firstLoop !== 0) {
+            pokemonHeldItems.innerText += ", "
+        } else {
+            pokemonHeldItems.innerText = "Held Items: "
+            firstLoop = 1;
+        };
+
+        pokemonHeldItems.innerText += element.item.name;
+
+    })
 
 
 };
